3bb5bb806f9d2393b71e4b9534e69162e0a30b55
jorge <jorgeantoniocv@hotmail.com>
2020-10-29 12:56:18 -0600

    Nueva Imagen y archivo modificado

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,1 +1,12 @@
+import cv2
 
+img= cv2.imread('Imagen.jpg')
+img= cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+print(img.shape)
+print(img[0][0])
+img= cv2.resize(img,(256,256))
+
+#Solo para linux 
+cv2.imwrite("ResizeImagen.jpg",img)
+cv2.imshow('Imagen',img)
+cv2.waitKey(0)
