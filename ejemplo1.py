ea32e1290e6435319c22e7de41e69815be98b2c1
jorge <jorgeantoniocv@hotmail.com>
2020-10-29 13:11:01 -0600

    Escala de Grises y modificacion ejemplo1.py

--- ejemplo1.py
+++ ejemplo1.py
@@ -7,6 +7,16 @@ print(img[0][0])
 img= cv2.resize(img,(256,256))
 
 #Solo para linux 
+
 cv2.imwrite("ResizeImagen.jpg",img)
+img= cv2.imread('Imagen.jpg')
+img= cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+print(img.shape)
+print(img[0][0])
+
+cv2.imwrite('grayImagen.jpg',img)
+
+"""
 cv2.imshow('Imagen',img)
-cv2.waitKey(0)
+cv2.waitKey(0)"""
+
